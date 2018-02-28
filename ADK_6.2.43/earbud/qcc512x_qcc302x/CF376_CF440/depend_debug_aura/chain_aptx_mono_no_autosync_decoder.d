8b59a098f68bd7eda4ae09683a52af41293de5d6
chaw.meng <chaw.meng@geortek.com>
2018-02-28 18:30:30 +0800

    Delete full temp project

--- ADK_6.2.43/earbud/qcc512x_qcc302x/CF376_CF440/depend_debug_aura/chain_aptx_mono_no_autosync_decoder.d
+++
@@ -1,3 +0,0 @@
-depend_debug_aura/chain_aptx_mono_no_autosync_decoder.o: ..\..\chains\chain_aptx_mono_no_autosync_decoder.c \
-    ..\..\chains\chain_aptx_mono_no_autosync_decoder.h \
-    ..\..\chains\..\av_headset_chain_roles.h 
--- ADK_6.2.43/earbud/qcc512x_qcc302x/CF376_CF440/depend_debug_aura/chain_forwarding_input_aac_left.d
+++
@@ -1,3 +0,0 @@
-depend_debug_aura/chain_forwarding_input_aac_left.o: ..\..\chains\chain_forwarding_input_aac_left.c \
-    ..\..\chains\chain_forwarding_input_aac_left.h \
-    ..\..\chains\..\av_headset_chain_roles.h 
