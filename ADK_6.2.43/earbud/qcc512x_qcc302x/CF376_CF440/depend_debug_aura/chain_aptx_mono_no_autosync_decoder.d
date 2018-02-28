663680303c428209dbc3ddf6cfa0559e363f343a
chaw.meng <chaw.meng@geortek.com>
2018-02-28 18:21:00 +0800

    One project can be built should include full SDK, so first delete temp project

---
+++ ADK_6.2.43/earbud/qcc512x_qcc302x/CF376_CF440/depend_debug_aura/chain_aptx_mono_no_autosync_decoder.d
@@ -0,0 +1,3 @@
+depend_debug_aura/chain_aptx_mono_no_autosync_decoder.o: ..\..\chains\chain_aptx_mono_no_autosync_decoder.c \
+    ..\..\chains\chain_aptx_mono_no_autosync_decoder.h \
+    ..\..\chains\..\av_headset_chain_roles.h 
---
+++ ADK_6.2.43/earbud/qcc512x_qcc302x/CF376_CF440/depend_debug_aura/chain_forwarding_input_aac_left.d
@@ -0,0 +1,3 @@
+depend_debug_aura/chain_forwarding_input_aac_left.o: ..\..\chains\chain_forwarding_input_aac_left.c \
+    ..\..\chains\chain_forwarding_input_aac_left.h \
+    ..\..\chains\..\av_headset_chain_roles.h 
